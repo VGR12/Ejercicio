d434bc620af3bea62a659c9812d752d4adf2cabf
VGR12 <victorguillenramirez213@gmail.com>
2022-08-18 23:59:59 -0600

    Actualizando

---
+++ Service/Services/InterfaceServicePosts.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Services.Services
+{
+    public interface InterfaceServicePosts
+    {
+        public Task<List<Models.Model.Post>> GetListPosts();
+    }
+}
